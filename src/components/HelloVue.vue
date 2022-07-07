485b82837b99f02e2d30d03d906e613611f4e76c
unknown <yangyt@DCFUNDOA.COM>
2022-07-07 20:23:20 +0800

    做了table样式

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,167 @@
+ <template>
+  <div class="user-table">
+    <div class="header-query-box">
+      <div class="left-box">
+        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+          <el-form-item>
+            <el-input v-model="formInline.user" placeholder="搜索"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" icon="el-icon-search">查询</el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" icon="el-icon-plus">删除</el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="danger" icon="el-icon-delete">删除</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="right-box"></div>
+    </div>
+    <div class="table-detail">
+      <el-table :fit='true'
+        ref="multipleTable"
+        :data="tableData"
+        border
+        tooltip-effect="dark"
+         style="width: 98%;"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column  type="selection" > </el-table-column>
+         <el-table-column prop="num" label="序号" >
+        </el-table-column>
+        <el-table-column  label="日期"  >
+          <template slot-scope="scope">{{ scope.row.date }}</template>
+        </el-table-column>
+       <el-table-column prop="name" label="姓名"  >
+        </el-table-column>
+         <el-table-column prop="like" label="喜欢"  >
+        </el-table-column>
+         <el-table-column prop="title" label="标题"  >
+        </el-table-column>
+        <el-table-column prop="address" label="地址"  show-overflow-tooltip>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+ <script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+         num:"1",
+          date: "2016-05-02",
+          name: "王小虎",
+          title:"beautiful",
+          like:"1000",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          num:"1",
+          date: "2016-05-02",
+          name: "王小虎",
+          title:"beautiful",
+          like:"1000",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+         num:"1",
+          date: "2016-05-02",
+          name: "王小虎",
+          title:"beautiful",
+          like:"1000",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+         num:"1",
+          date: "2016-05-02",
+          name: "王小虎",
+          title:"beautiful",
+          like:"1000",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          num:"1",
+          date: "2016-05-02",
+          name: "王小虎",
+          title:"beautiful",
+          like:"1000",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          num:"1",
+          date: "2016-05-02",
+          name: "王小虎",
+          title:"beautiful",
+          like:"1000",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          num:"1",
+          date: "2016-05-02",
+          name: "王小虎",
+          title:"beautiful",
+          like:"1000",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+       multipleSelection: [],
+      formInline: {
+        user: "",
+        region: "",
+      },
+    };
+  },
+  methods: {
+     toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+    onSubmit() {
+      console.log("submit!");
+    },
+  },
+};
+</script>
+<style scoped>
+.user-table {
+  width: 100%;
+  height: 100%;
+  /* background: blue; */
+  display: flex;
+  flex-direction: column;
+}
+.left-box,
+.right-box {
+  padding:1%;
+  height: 100%;
+  width: 50%;
+}
+.header-query-box {
+  height: 9%;
+  display: flex;
+  justify-content: flex-start;
+}
+.left-box {
+  display: flex;
+  justify-content: flex-start;
+}
+.table-detail {
+  width: 100%;
+  height: 300px;
+  padding:1%
+}
+
+</style>
---
+++ src/view/indexView.vue
@@ -0,0 +1,126 @@
+<template>
+  <div style="width:100%;height:100%">
+    <el-container>
+    <el-aside width="210px">
+      <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+    
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>首页</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1">看板
+            <router-link to="HelloVue"></router-link>
+          </el-menu-item>
+          <el-menu-item index="1-2">工作台</el-menu-item>
+        </el-menu-item-group>
+          <el-menu-item index="1-3">仓库</el-menu-item>
+        <el-submenu index="1-4">
+          <template slot="title">组件</template>
+          <el-menu-item index="1-4-1">图标</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">其他</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <i class="el-icon-document"></i>
+        <span slot="title">图表</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">通知</span>
+      </el-menu-item>
+    </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>
+        <div class="main-container">
+        <HelloVue></HelloVue>
+        </div>
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
+  </div>
+</template>
+
+<script>
+// import logo from "@/components/headLogo.vue";
+import HelloVue from "@/components/HelloVue.vue"
+export default {
+  name: "index",
+  components: { HelloVue },
+  data() {
+    return {
+      isCollapse: true,
+    };
+  },
+
+  // components: {},
+
+  computed: {},
+
+  mounted: {
+  
+  },
+
+  methods: {},
+};
+</script>
+<style scoped>
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: left;
+    line-height: 200px;
+    overflow: hidden;
+  }
+  .el-menu {
+    height:100%
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    /* line-height: 160px; */
+     height:100%;
+     /* background:red; */
+  }
+.main-container{
+  width:100%;
+  height:100%;
+  background:white;
+}
+  .el-container {
+    /* margin-bottom: 40px; */
+    height: 100%;
+  }
+ 
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
+  
+</style>
